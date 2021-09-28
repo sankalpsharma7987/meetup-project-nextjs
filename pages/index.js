7499e78f48dd69f853b343212dc0ebad5afe36e7
sankalpsharma7987@gmail.com <sankalpsharma7987@gmail.com>
2021-09-28 12:49:09 -0400

    Code comments for index file where all meetup data is rendered using useEffect

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,9 @@ const HomePage = () => {
   useEffect(() => {
     //Simulate experience of fetching and loading data in loadedData state
     // Assume DUMMY_MEETUPS data was fetched from an API
+    /*This implementation will render an empty meetup list in the page source code as the page, when pre-rendered by NextJS, will not have load data in loadedData state
+      As this page is pre-rendered and send to the client, the browser will then run the useEffect after the page is rendered for the first time in order to  load data. 
+      Thus the meetups data is not pre-rendered and send to the client by NextJS will this implementation*/
     setLoadedData(DUMMY_MEETUPS);
   }, []);
   return <MeetupList meetups={loadedData} />;
